f019dc1a5b2705e72e69069bed7881bd0e5d2767
Swantan Barua <swantan@relentless.ventures>
2024-03-12 01:32:13 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Swantan Barua on 12/03/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
