ad988d23ad8995df0cc5c6b8bd25d9e73add01d3
Swantan Barua <work.swantan@gmail.com>
2024-03-12 01:37:49 +0530

    Home View and Onboarding View introduced to the project

---
+++ Restart/View/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  Restart
+//
+//  Created by Swantan Barua on 12/03/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    // MARK: - BODY
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ Restart/View/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Swantan Barua on 12/03/24.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    
+    // MARK: - BODY VIEW
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
